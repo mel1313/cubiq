d57736ea31d7a84cedee707c15f289bcaf23a7dc
mel1313 <melvinleil@gmail.com>
2016-01-04 11:39:32 +0800

    added static

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static("index.html"));
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
